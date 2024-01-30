b36a2086753c679a8eb067af50b341c3b3f87609
belassiria aymane <aymanebel2@outlook.fr>
2024-01-30 16:01:10 +0100

    firts push

---
+++ src/test/java/com/example/youreview/YouReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.youreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
