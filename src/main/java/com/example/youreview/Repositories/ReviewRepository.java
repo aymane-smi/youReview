2789ec2fc32350006d9e5114d9226588cf1cf1be
belassiria aymane <aymanebel2@outlook.fr>
2024-01-30 17:59:48 +0100

    add repsoitory annotation

--- src/main/java/com/example/youreview/Repositories/ReviewRepository.java
+++ src/main/java/com/example/youreview/Repositories/ReviewRepository.java
@@ -3,9 +3,11 @@ package com.example.youreview.Repositories;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.youreview.Models.Entites.Review;
 
+@Repository
 public interface ReviewRepository extends JpaRepository<Review, UUID>{
     
 }
--- src/main/java/com/example/youreview/Repositories/UserRepository.java
+++ src/main/java/com/example/youreview/Repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.example.youreview.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.youreview.Models.Entites.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
     
 }
