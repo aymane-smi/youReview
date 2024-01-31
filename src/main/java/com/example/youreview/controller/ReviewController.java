ab158807a56923f75f36258fb7bd95bdc88be8e4
alaeloula <105067668+alaeloula@users.noreply.github.com>
2024-01-31 11:33:00 +0100

    add review controller

--- src/main/java/com/example/youreview/controller/ReviewController.java
+++ src/main/java/com/example/youreview/controller/ReviewController.java
@@ -1,4 +1,63 @@
 package com.example.youreview.controller;
 
+import com.example.youreview.Models.Entites.Review;
+import com.example.youreview.Services.ReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@Controller
+@RequestMapping("/reviews")
 public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    @Autowired
+    public ReviewController(ReviewService reviewService) {
+        this.reviewService = reviewService;
+    }
+
+    @GetMapping("/create")
+    public String showCreateReviewForm(Model model) {
+        model.addAttribute("review", new Review());
+        return "createReview";
+    }
+
+    @PostMapping("/create")
+    public String createReview(@ModelAttribute Review review) {
+        reviewService.saveReview(review);
+        return "redirect:/reviews";
+    }
+
+    @GetMapping("/{id}")
+    public String showReviewDetails(@PathVariable UUID id, Model model) {
+        Review review = reviewService.getReviewById(id);
+        if (review != null) {
+            model.addAttribute("review", review);
+            return "reviewDetails";
+        } else {
+            return "notFound";
+        }
+    }
+
+    @GetMapping
+    public String showAllReviews(Model model) {
+        List<Review> reviews = reviewService.getAllReviews();
+        model.addAttribute("reviews", reviews);
+        return "allReviews";
+    }
+
+    @GetMapping("/{id}/delete")
+    public String deleteReview(@PathVariable UUID id) {
+        reviewService.deleteReview(id);
+        return "redirect:/reviews";
+    }
+
+
+
 }
+
---
+++ src/main/java/com/example/youreview/controller/UserController.java
@@ -0,0 +1,4 @@
+package com.example.youreview.controller;
+
+public class UserController {
+}
