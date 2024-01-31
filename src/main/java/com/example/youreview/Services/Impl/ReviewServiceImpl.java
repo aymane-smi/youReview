a8ff6c1b6ee43bb8cd3ac0d11d0dcad5a5d36c2f
alaeloula <105067668+alaeloula@users.noreply.github.com>
2024-01-31 11:14:18 +0100

    add review service

--- src/main/java/com/example/youreview/Services/Impl/ReviewServiceImpl.java
+++ src/main/java/com/example/youreview/Services/Impl/ReviewServiceImpl.java
@@ -1,11 +1,36 @@
 package com.example.youreview.Services.Impl;
 
+import com.example.youreview.Models.Entites.Review;
 import com.example.youreview.Repositories.ReviewRepository;
 import com.example.youreview.Services.ReviewService;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.UUID;
+@Service
 public class ReviewServiceImpl implements ReviewService{
     private final ReviewRepository reviewRepository;
     public ReviewServiceImpl(ReviewRepository reviewRepository){
         this.reviewRepository = reviewRepository;
+    }
+
+    @Override
+    public Review saveReview(Review review) {
+        return reviewRepository.save(review);
+    }
+
+    @Override
+    public Review getReviewById(UUID id) {
+        return reviewRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<Review> getAllReviews() {
+        return reviewRepository.findAll();
+    }
+
+    @Override
+    public void deleteReview(UUID id) {
+        reviewRepository.deleteById(id);
     }
 }
--- src/main/java/com/example/youreview/Services/ReviewService.java
+++ src/main/java/com/example/youreview/Services/ReviewService.java
@@ -1,5 +1,16 @@
 package com.example.youreview.Services;
 
+import com.example.youreview.Models.Entites.Review;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface ReviewService {
-    
+    Review saveReview(Review review);
+
+    Review getReviewById(UUID id);
+
+    List<Review> getAllReviews();
+
+    void deleteReview(UUID id);
 }
