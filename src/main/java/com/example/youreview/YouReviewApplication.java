64660655cb947a3ab97f28cb4928e5ebd4300cc4
belassiria aymane <aymanebel2@outlook.fr>
2024-02-03 20:09:40 +0100

    making pages

--- src/main/java/com/example/youreview/YouReviewApplication.java
+++ src/main/java/com/example/youreview/YouReviewApplication.java
@@ -1,12 +1,7 @@
  package com.example.youreview;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.example.youreview.Repositories.UserRepository;
-import com.example.youreview.Services.Impl.UserServiceImpl;
 
 @SpringBootApplication
 public class YouReviewApplication {
